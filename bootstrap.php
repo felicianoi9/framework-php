a6b86f0bdceb75a48900914c130b0045056480a4
Rogério Feliciano dos Santos <felicianoi9@gmail.com>
2021-02-25 15:47:49 -0300

    Start my own PHP Framework

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Felicianoi9\Framework\Router;
+
+$router = new Router();
---
+++ src/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Felicianoi9\Framework;
+
+class Router 
+{
+    public function handler()
+    {
+        return true;
+    }
+}
